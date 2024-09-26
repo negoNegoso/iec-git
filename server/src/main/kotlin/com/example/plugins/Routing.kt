4892034eb6f6e947af347b15131a68a3847a1c47
nicolle <nicollefujii@outlook.com>
2024-09-26 20:59:39 -0300

    Nicolle

--- server/src/main/kotlin/com/example/plugins/Routing.kt
+++ server/src/main/kotlin/com/example/plugins/Routing.kt
@@ -7,7 +7,7 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Ana Pupo, Eliharison, Ana Lima, Renato Hioji Okamoto Odake, Lauro")
+            call.respondText("Ana Pupo, Eliharison, Ana Lima, Renato Hioji Okamoto Odake, Lauro, Nicolle")
         }
     }
 }
