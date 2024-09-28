4ea53f8611e65e7bbd2446f441a252e1fd9729e1
Asher <asherndebortoli@gmail.com>
2024-09-27 22:35:04 -0300

    feat:resolve names

--- server/src/main/kotlin/com/example/plugins/Routing.kt
+++ server/src/main/kotlin/com/example/plugins/Routing.kt
@@ -7,7 +7,7 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Asher")
+            call.respondText("Ana Luíza, Asher")
 
         }
     }
